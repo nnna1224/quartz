8302576b890872094362ee0c52d4610001914bcf
nnna <nagidec.24a@gmail.com>
2025-07-20 21:30:49 +0900

    Quartz sync: Jul 20, 2025, 9:30 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,7 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
+    // Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
