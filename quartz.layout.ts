3e86bdd6d9fe64ff8c3f856f8d5f0284d24a7d49
nnna <nagidec.24a@gmail.com>
2025-07-20 20:20:53 +0900

    Quartz sync: Jul 20, 2025, 8:20 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,9 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.PageTitle(),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
